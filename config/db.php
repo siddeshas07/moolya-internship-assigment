28e37654901fcec94beab160e382787bf1e228ea
siddeshas07 <58839558+siddeshas07@users.noreply.github.com>
2021-05-07 19:26:24 +0530

    Update db.php
    remote database

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "crud");
+$conn = mysqli_connect("remotemysql.com", "BYUH85VYgd", "e6JL7jF0xB", "BYUH85VYgd");
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
